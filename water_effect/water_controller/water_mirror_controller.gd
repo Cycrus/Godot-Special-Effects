378142fc93e6b2b3753e9daf1e7fe522e804664c
Cycrus <marxcyril@gmail.com>
2025-02-09 12:33:54 +0100

    fix: Water mirror images now work with animated sprites. Also prepared the project for exporting.

--- water_effect/water_controller/water_mirror_controller.gd
+++ water_effect/water_controller/water_mirror_controller.gd
@@ -23,14 +23,40 @@ var mirrored_objects: Dictionary = {}
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if do_mirror:
-		for body in mirrored_objects:
-			var sprite = mirrored_objects[body]["sprite"]
-			var y_offset = mirrored_objects[body]["y_offset"]
-			sprite.global_position = body.global_position
-			sprite.global_position.y += sprite.texture.get_height() * sprite.global_scale.y + y_offset
+		_update_mirror_image()
 	else:
 		if mirrored_objects.size() > 0:
 			mirrored_objects.clear()
+
+
+func _update_mirror_image() -> void:
+	for body in mirrored_objects:
+		var sprite = mirrored_objects[body]["sprite"]
+		var original_sprite = mirrored_objects[body]["original_sprite"]
+		var y_offset = mirrored_objects[body]["y_offset"]
+		sprite.global_position = body.global_position
+		
+		# TODO: Manually copy the frame texture to the mirrored sprite
+		#       to avoid bleeding in of other animation frames.
+		var sprite_texture: CompressedTexture2D = original_sprite.texture
+		var texture_size: Vector2 = sprite_texture.get_size()
+		var frame_size: Vector2 = Vector2(texture_size.x / original_sprite.hframes,
+										  texture_size.y / original_sprite.vframes)
+		var curr_frame_pos : Vector2 = Vector2(frame_size.x * original_sprite.frame_coords.x, frame_size.y * original_sprite.frame_coords.y)
+		var original_texture: CompressedTexture2D = original_sprite.texture
+		var original_image: Image = original_texture.get_image()
+		var frame_rect: Rect2i = Rect2i(curr_frame_pos, frame_size)
+		var cropped_image: Image = original_image.get_region(frame_rect)
+		var cropped_texture: ImageTexture = ImageTexture.create_from_image(cropped_image)
+		sprite.texture = cropped_texture
+		
+		
+		sprite.global_position.y += sprite.texture.get_height() * sprite.global_scale.y + y_offset
+		sprite.hframes = 1
+		sprite.vframes = 1
+		sprite.frame = 0
+		sprite.frame_coords = Vector2(0, 0)
+		sprite.skew = original_sprite.skew * -1
 
 
 func _add_mirrored_object(body: Node2D) -> void:
@@ -44,6 +70,7 @@ func _add_mirrored_object(body: Node2D) -> void:
 			return
 		mirrored_objects[body] = {
 			"sprite": sprite.duplicate(),
+			"original_sprite": sprite,
 			"y_offset": 0.0
 		}
 		add_child(mirrored_objects[body]["sprite"])
--- wind_effect/example_resources/player_behavior.gd
+++ wind_effect/example_resources/player_behavior.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DisplayServer.mouse_set_mode(DisplayServer.MouseMode.MOUSE_MODE_HIDDEN)
+	$AnimationPlayer.play("idle")
 	
 
 
